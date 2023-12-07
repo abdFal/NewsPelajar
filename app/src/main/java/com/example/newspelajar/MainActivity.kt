b2abe263d2e6cdb759aea54b4bac760ba8ff2746
abdfal <naufaldec07@gmail.com>
2023-12-07 21:05:18 +0700

    Buat drawer

--- app/src/main/java/com/example/newspelajar/MainActivity.kt
+++ app/src/main/java/com/example/newspelajar/MainActivity.kt
@@ -2,8 +2,12 @@ package com.example.newspelajar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
 import androidx.activity.viewModels
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.viewpager2.widget.ViewPager2
 import com.example.newspelajar.adapter.NewsAdapter
@@ -17,6 +21,9 @@ import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
+    private lateinit var btnOpenDrawer: ImageButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
@@ -24,6 +31,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         val tabLayout: TabLayout = binding.tabLayout
         val viewPager: ViewPager2 = binding.viewPager
+
 
         val pagerAdapter = PagerAdapter(this)
         viewPager.adapter = pagerAdapter
@@ -35,6 +43,21 @@ class MainActivity : AppCompatActivity() {
                 2 -> tab.text = "Tribun"
             }
         }.attach()
+        drawerLayout = findViewById(R.id.drawer_layout)
+        btnOpenDrawer = findViewById(R.id.btnOpenDrawer)
+
+        actionBarDrawerToggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(actionBarDrawerToggle)
+        // Set up click listener for the ImageButton
+        btnOpenDrawer.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(view: View) {
+                if (drawerLayout.isDrawerOpen(binding.navigationView)) {
+                    drawerLayout.closeDrawer(binding.navigationView)
+                } else {
+                    drawerLayout.openDrawer(binding.navigationView)
+                }
+            }
+        })
 
     }
     }
